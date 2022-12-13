8bc648e1b5441b673878a54950d12b346c18e889
okdbsn <120519374+okdbsn@users.noreply.github.com>
2022-12-13 22:55:18 +0100

    Add files via upload

--- Gierkakunda.py
+++ Gierkakunda.py
@@ -10,38 +10,38 @@ print("="*100)
 life = 100
 umiejetnosci = 1000
 # ========================================CIOSY==============================================
-def zapierdoliles_mu_kopa():
+def grzecznie_prosisz_zeby_ustapil():
     return randint(30, 50)
 
-def wykreciles_cysia():
+def wykreciles_mu_uszko():
     global umiejetnosci
     umiejetnosci -= 15
     return randint(10, 12)
 
-def dusisz_go():
+def laskoczesz_go():
     global umiejetnosci
     umiejetnosci -= 25
     return randint(15, 18)
 
 def wybierz_jak_zaatakujesz():
     print(f"{nick} wybierz atak:")
-    print("P - zajepierdoliles mu kopa")
-    print("H - wykreciles cysia")
-    print("O - dusisz go")
+    print("P - grzecznie prosisz zeby ustapil")
+    print("H - wykreciles mu uszko")
+    print("O - laskoczesz go")
     jaki = input()
     if jaki.upper() == "P":
-        return zapierdoliles_mu_kopa()
+        return grzecznie_prosisz_zeby_ustapil()
     
     elif jaki.upper() == "H":
         if umiejetnosci >= 12:
-            return wykreciles_cysia()
+            return wykreciles_mu_uszko()
         else:
             print("Nie możesz teraz tego użyć :C")
             return Kunda
     
     elif jaki.upper() == "O":
         if umiejetnosci >= 18:
-            return dusisz_go()
+            return laskoczesz_go()
         else:
             print("Nie możesz teraz tego użyć :C")
             return 0
@@ -51,7 +51,7 @@ def wybierz_jak_zaatakujesz():
         print("Zostajesz zaatakowany, następnym razem wybierz dobrze")
         print("="*100)   
         return 0
-# =======================================RODZINKA=========================================
+# =======================================Boss=========================================
 # Kundzior = ["nazwa", ile ma hp, ile ci zabiera hp]
 Kunda = ["Kunda",120, 15]
 
@@ -59,7 +59,7 @@ Kunda = ["Kunda",120, 15]
 
 while True:
     print(f"{nick} musisz walczyć z kunda! Podejmujesz się walki?")
-    print("Tak - 1, Odpuszczam jak cipa - 2")
+    print("Tak - 1, Odpuszczam jak lamus - 2")
     print("="*100)
 
     inp = input()
@@ -67,7 +67,7 @@ while True:
         print("Walczysz z Kunda!")
         break
     elif inp == "2":
-        print("jestes beznadziejny spierdalaj wroc jak zmezniejesz")
+        print("jestes beznadziejny wroc jak zmezniejesz")
         print("="*100)
         break
     else:
